fec41150901dc7d3d0ebec505026c86942a3d0e5
vikashkaushik <vikakau@gmail.com>
2021-06-30 11:31:39 -0500

    test message

--- src/AnalogOutputs.ino
+++ src/AnalogOutputs.ino
@@ -4,15 +4,17 @@
  * Author:
  * Date:
  */
-
+SYSTEM_MODE(MANUAL);
+SYSTEM_THREAD(ENABLED);
 // setup() runs once, when the device is first turned on.
 void setup() {
   // Put initialization like pinMode and begin functions here.
-
+  pinMode(D6, OUTPUT); 
 }
 
 // loop() runs over and over again, as quickly as it can execute.
 void loop() {
   // The core of your code will likely live here.
-
+  
+  analogWrite(D6, 128);
 }
