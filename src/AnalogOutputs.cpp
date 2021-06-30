4d3a31cb38abfb56e8ae16404ce14a5d9c025c89
vikashkaushik <vikakau@gmail.com>
2021-06-30 17:04:31 -0500

    final commit

---
+++ src/AnalogOutputs.cpp
@@ -0,0 +1,75 @@
+/******************************************************/
+//       THIS IS A GENERATED FILE - DO NOT EDIT       //
+/******************************************************/
+
+#include "Particle.h"
+#line 1 "c:/Users/Vikash/Downloads/AnalogOutputs/src/AnalogOutputs.ino"
+/*
+ * Project AnalogOutputs
+ * Description:
+ * Author: Vikash Kaushik
+ * Date:6/30/21
+ */
+void setup();
+void loop();
+#line 7 "c:/Users/Vikash/Downloads/AnalogOutputs/src/AnalogOutputs.ino"
+SYSTEM_MODE(MANUAL);
+SYSTEM_THREAD(ENABLED);
+// setup() runs once, when the device is first turned on.
+void setup() {
+  // Put initialization like pinMode and begin functions here.
+  pinMode(D5, OUTPUT);
+  pinMode(D6, OUTPUT); 
+  pinMode(D7, OUTPUT);
+}
+
+// loop() runs over and over again, as quickly as it can execute.
+void loop() {
+  // The core of your code will likely live here.
+  
+  // red
+  analogWrite(D7, 255);
+  analogWrite(D6,0); // turns it on 
+  analogWrite(D5,0);
+  delay(1000); // delay of 1 second
+
+  // green
+  analogWrite(D7, 0);
+  analogWrite(D6,255); // turns it on 
+  analogWrite(D5,0);
+  delay(1000); // delay of 1 second
+
+
+  // blue
+  analogWrite(D7, 0);
+  analogWrite(D6,0); // turns it on 
+  analogWrite(D5,255);
+  delay(1000); // delay of 1 second
+
+  // orange
+  analogWrite(D7, 255);
+  analogWrite(D6,15); // turns it on 
+  analogWrite(D5,0);
+  delay(1000); // delay of 1 second
+
+  // yellow
+  analogWrite(D7, 255);
+  analogWrite(D6,95); // turns it on 
+  analogWrite(D5,0);
+  delay(1000); // delay of 1 second
+
+  // purple
+  analogWrite(D7, 106);
+  analogWrite(D6,13); // turns it on 
+  analogWrite(D5,173);
+  delay(1000); // delay of 1 second
+
+  // cyan
+  analogWrite(D7, 0);
+  analogWrite(D6,255); // turns it on 
+  analogWrite(D5,255);
+  delay(1000); // delay of 1 second
+
+
+ 
+}
--- src/AnalogOutputs.ino
+++ src/AnalogOutputs.ino
@@ -1,20 +1,66 @@
 /*
  * Project AnalogOutputs
  * Description:
- * Author:
- * Date:
+ * Author: Vikash Kaushik
+ * Date:6/30/21
  */
 SYSTEM_MODE(MANUAL);
 SYSTEM_THREAD(ENABLED);
 // setup() runs once, when the device is first turned on.
 void setup() {
   // Put initialization like pinMode and begin functions here.
+  pinMode(D5, OUTPUT);
   pinMode(D6, OUTPUT); 
+  pinMode(D7, OUTPUT);
 }
 
 // loop() runs over and over again, as quickly as it can execute.
 void loop() {
   // The core of your code will likely live here.
   
-  analogWrite(D6, 128);
+  // red
+  analogWrite(D7, 255);
+  analogWrite(D6,0); // turns it on 
+  analogWrite(D5,0);
+  delay(1000); // delay of 1 second
+
+  // green
+  analogWrite(D7, 0);
+  analogWrite(D6,255); // turns it on 
+  analogWrite(D5,0);
+  delay(1000); // delay of 1 second
+
+
+  // blue
+  analogWrite(D7, 0);
+  analogWrite(D6,0); // turns it on 
+  analogWrite(D5,255);
+  delay(1000); // delay of 1 second
+
+  // orange
+  analogWrite(D7, 255);
+  analogWrite(D6,15); // turns it on 
+  analogWrite(D5,0);
+  delay(1000); // delay of 1 second
+
+  // yellow
+  analogWrite(D7, 255);
+  analogWrite(D6,95); // turns it on 
+  analogWrite(D5,0);
+  delay(1000); // delay of 1 second
+
+  // purple
+  analogWrite(D7, 106);
+  analogWrite(D6,13); // turns it on 
+  analogWrite(D5,173);
+  delay(1000); // delay of 1 second
+
+  // cyan
+  analogWrite(D7, 0);
+  analogWrite(D6,255); // turns it on 
+  analogWrite(D5,255);
+  delay(1000); // delay of 1 second
+
+
+ 
 }
